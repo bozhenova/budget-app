7fad991793b460b991fe55eff3ed38298bcf46c7
Margo <margo.bozhenova@gmail.com>
2020-08-11 22:30:08 +0300

    Add BudgetListItem component

---
+++ src/components/BudgetListItem.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="list-item">
+<span class="budget-comment">{{ item.comment }}</span>
+  <span class="budget-value">{{ item.value }}</span>
+  <ElButton type="danger" size="mini" @click="handleDelete(item.id)">
+  Delete
+  </ElButton>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BudgetListItem',
+  props: {
+  item: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+    methods: {
+    handleDelete(id) {
+      this.$emit('delete-item', id);
+    }
+  }
+}
+</script>
+
+  <style scoped>
+.budget-value {
+  font-weight: bold;
+  margin-left: auto;
+  margin-right: 20px;
+}
+.list-item {
+  display: flex;
+  align-items: center;
+  padding: 10px 15px;
+}
+
+  </style>
