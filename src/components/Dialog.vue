edee2c89ee73f2ba601eb6bd6ca7cd9912dadd04
Margo <margo.bozhenova@gmail.com>
2020-08-17 11:46:44 +0300

    Make Dialog a separate component

---
+++ src/components/Dialog.vue
@@ -0,0 +1,40 @@
+<template>
+ <ElDialog
+      title="Do you want to delete this item?"
+      :visible="this.dialogVisible"
+      width="30%"
+      :before-close="closeDialog"
+    >
+      <span>
+        <ElButton @click="closeDialog">Cancel</ElButton>
+        <ElButton type="primary" @click="handleConfirm">Confirm</ElButton>
+      </span>
+    </ElDialog>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'Dialog',
+  computed: {
+      ...mapGetters('data', ['dialogVisible', 'currentId']),
+  },
+  methods: {
+    ...mapActions('data', ['toggleDialog', 'deleteItem']),
+      closeDialog() {
+      this.toggleDialog(false);
+    },
+      handleConfirm() {
+      this.toggleDialog(false);
+      this.deleteItem(this.currentId)
+    },
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
